2eaa9d9d9c45aa23cede32551f375348fbd87c54
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-04-09 13:58:50 +0000

    Solve 3046 problem of beakjoon

---
+++ src/beakjoon/3046/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int R1, S;
+int R2;
+
+int main(void) {
+    scanf("%d %d", &R1, &S);
+    
+    R2 =  2 * S - R1;
+
+    printf("%d\n", R2);
+    return 0;
+}
