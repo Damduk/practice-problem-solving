c018923f63a6b7a7ddd5c63f48a148af1ce14103
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2020-03-24 10:55:19 +0000

    Fix 2583 problem of boj

--- src/beakjoon/2583/main.cpp
+++ src/beakjoon/2583/main.cpp
@@ -1,67 +1,68 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define ON 1
-#define OFF 0
-
 using namespace std;
 
-int d[4][2] = {{ -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 }};
-int board[110][110];
-int visited[110][110];
 int M, N, K;
-int X1, X2, Y1, Y2;
-vector<int> area;
+int A[105][105];
+vector<int> rect;
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1};
 
-int setOn(int x1, int y1, int x2, int y2) {
-    for (int i = y1; i < y2; i++) 
-        for (int j = x1; j < x2; j++)
-            board[i][j] = ON;
+int isOut(int x, int y) {
+    return x < 0 || y < 0 || x >= N || y >= M;
 }
 
-int isValid(int x, int y) {
-    return x >= 0 && x < M && y >= 0 && y < N;
+int dfs(int x, int y) {
+    A[y][x] = 2;
+    
+    int res = 1; 
+    for (int i = 0; i < 4; i++) {
+        int nextx = x + dx[i];
+        int nexty = y + dy[i];
+
+        if (isOut(nextx, nexty) || A[nexty][nextx] > 0) {
+            continue;
+        }
+        
+        res += dfs(nextx, nexty); 
+    } 
+    return res;
 }
 
-int getArea(int x, int y) {
-    if (!isValid(x, y)) return 0;
-    if (visited[x][y]) return 0;
-    if (board[x][y] == ON) return 0;
-
-    visited[x][y] = 1;
-
-    int result = 1;
-    for (int i = 0; i < 4; i++)
-        result += getArea(x + d[i][0], y + d[i][1]);
-
-    return result;
-}
-
-void solve() {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            int v = getArea(j, i);
-            if (v > 0)
-                area.push_back(v);
+void cover(int x1, int y1, int x2, int y2) {
+    for (int i = y1; i < y2; i++) {
+        for (int j = x1; j < x2; j++) {
+            A[i][j] = 1;
         }
     }
-
-    sort(area.begin(), area.end());
 }
 
-int main(void) {
+int main() {
     scanf("%d %d %d", &M, &N, &K);
-
     for (int i = 0; i < K; i++) {
-        scanf("%d %d %d %d", &X1, &Y1, &X2, &Y2);
-        setOn(X1, Y1, X2, Y2);
+        int x1, y1, x2, y2;
+        scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+        cover(x1, y1, x2, y2);
     }
-
-    solve();
-
-    printf("%d\n", area.size());
-    for (int& item : area)
+ 
+    int res = 0;
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            if (A[i][j] > 0) {
+                continue;
+            }
+            
+            res++;
+            rect.push_back(dfs(j, i));
+        }
+    }
+  
+    sort(rect.begin(), rect.end());
+    
+    printf("%d\n", res);
+    for (auto item : rect) {
         printf("%d ", item);
-
-    return 0;
+    }
+	return 0;
 }
